d2587597079f09914394738271f998e91b688430
Alikhan <tuxubaev17@gmail.com>
2023-02-03 21:37:17 +0600

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package emailapp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Email em1 = new Email("Shamkhan", "Dandayev");
+
+        System.out.println(em1.showInfo());
+    }
+}
